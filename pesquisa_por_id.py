3ccd36d7203caa32ad912704deb9e266f156c141
Herkrad <54071521+Herkrad@users.noreply.github.com>
2022-06-01 19:14:27 -0300

    xyz

--- pesquisa_por_id.py
+++ pesquisa_por_id.py
@@ -14,45 +14,48 @@ schema:
 
 """
 
+
 class SearchById(object):
     def __init__(self, search_id=1, json_api=None):
         api = f"https://servicodados.ibge.gov.br/api/v1/pesquisas/{search_id}"
         self.json_api = requests.get(api).json()
-    
+
     def api_data(self):
         self.data_api = self.json_api
         return self.data_api
 
     def get_nome(self):
         self.api_data()
-        return self.data_api['nome']
+        return self.data_api["nome"]
 
     def get_id(self):
         self.api_data()
-        return self.data_api['id']
+        return self.data_api["id"]
 
     def get_observacao(self):
         self.api_data()
-        return self.data_api['observacao']
+        return self.data_api["observacao"]
 
     def get_contexto(self):
         self.api_data()
-        return self.data_api['contexto']
-    
+        return self.data_api["contexto"]
+
     def get_descricao(self):
         self.api_data()
-        return self.data_api['descricao']
-    
+        return self.data_api["descricao"]
+
     def get_data_periodos(self):
         self.api_data()
-        return self.data_api['periodos']
-    
+        return self.data_api["periodos"]
+
     def separate_periods(self):
-        self.fonte = list(map(lambda dat: dat['fonte'], self.get_data_periodos()))
-        self.nota = list(map(lambda dat: dat['nota'], self.get_data_periodos()))
-        self.periodo = list(map(lambda dat: dat['periodo'], self.get_data_periodos()))
-        self.publicacao = list(map(lambda dat: dat['publicacao'], self.get_data_periodos()))
-        self.versao = list(map(lambda dat: dat['versao'], self.get_data_periodos()))
+        self.fonte = list(map(lambda dat: dat["fonte"], self.get_data_periodos()))
+        self.nota = list(map(lambda dat: dat["nota"], self.get_data_periodos()))
+        self.periodo = list(map(lambda dat: dat["periodo"], self.get_data_periodos()))
+        self.publicacao = list(
+            map(lambda dat: dat["publicacao"], self.get_data_periodos())
+        )
+        self.versao = list(map(lambda dat: dat["versao"], self.get_data_periodos()))
         return self.fonte, self.nota, self.periodo, self.publicacao, self.versao
 
     def get_period_fonte(self):
@@ -70,8 +73,7 @@ class SearchById(object):
     def get_period_publicao(self):
         self.separate_periods()
         return self.publicacao
-    
+
     def get_period_versao(self):
         self.separate_periods()
         return self.versao
-        
